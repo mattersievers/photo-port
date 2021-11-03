8c67aeb3580b84cb4c04dfe3aaadc697638716f0
Matthew Sievers <mattersievers@gmail.com>
2021-11-02 23:52:11 -0500

    added form handlers and submission

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import Gallery from './components/Gallery';
 
 import './App.css';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -26,6 +27,7 @@ function App() {
       currentCategory = {currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery
         currentCategory = {currentCategory}
         ></Gallery>
